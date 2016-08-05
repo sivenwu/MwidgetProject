e75b5beaca6a16a532bea27a1ee967dc6f9e7f4f
wusy1220 <957562677@qq.com>
2016-08-05 15:14:36 +0800

    初始化工程

---
+++ app/src/main/java/androidm/wsy/cn/mwidgetproject/MainActivity.java
@@ -0,0 +1,13 @@
+package androidm.wsy.cn.mwidgetproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
